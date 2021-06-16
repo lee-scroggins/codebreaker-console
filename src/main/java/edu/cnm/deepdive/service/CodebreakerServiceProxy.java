1cba9e1b0e4e08e4c634421928cd7268c30ba36c
Lee Scroggins <lscrggns@yahoo.com>
2021-06-16 10:28:47 -0600

    Implemented game repository exceptions.

--- src/main/java/edu/cnm/deepdive/service/CodebreakerServiceProxy.java
+++ src/main/java/edu/cnm/deepdive/service/CodebreakerServiceProxy.java
@@ -3,6 +3,9 @@ package edu.cnm.deepdive.service;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import edu.cnm.deepdive.model.Game;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import okhttp3.logging.HttpLoggingInterceptor.Level;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -17,6 +20,7 @@ public interface CodebreakerServiceProxy {
   static CodebreakerServiceProxy getInstance() {
     return InstanceHolder.INSTANCE;
   }
+
   class InstanceHolder {
 
     private static final CodebreakerServiceProxy INSTANCE;
@@ -26,9 +30,15 @@ public interface CodebreakerServiceProxy {
           .excludeFieldsWithoutExposeAnnotation()
           .setDateFormat("yyyy-MM-dd-'T'HH:mm:ss.SSSZ")
           .create();
+      HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+      interceptor.setLevel(Level.BODY);
+      OkHttpClient client = new OkHttpClient.Builder()
+          .addInterceptor(interceptor)
+          .build();
       Retrofit retrofit = new Retrofit.Builder()
           .baseUrl("https://ddc-java.services/codebreaker/")
           .addConverterFactory(GsonConverterFactory.create(gson))
+          .client(client)
           .build();
       INSTANCE = retrofit.create(CodebreakerServiceProxy.class);
     }
